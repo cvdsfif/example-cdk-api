31f6f7aa3a61b4f6b52c7f016d64858ad90b3f9c
Nikit Zykov <nikit@zykov.com>
2024-03-16 16:13:36 +0100

    Project initialized

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
+  roots: ['<rootDir>/tests'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  }
-};
+  },
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['!tests/*', '!**/dist/**/*', '!tests/**/*'],
+  coverageReporters: ['json-summary', 'text']
+}
